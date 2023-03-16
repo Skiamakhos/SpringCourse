bbb2f0d51ca268e415fcf03e707328fc8504fe21
MattMoranJava <26431556+MattMoranJava@users.noreply.github.com>
2023-03-16 21:00:13 +0000

    Added a controller, service and view for Authors.

---
+++ src/main/java/com/skiamakhos/spring6webapp/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package com.skiamakhos.spring6webapp.controllers;
+
+import com.skiamakhos.spring6webapp.services.AuthorService;
+import com.skiamakhos.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @RequestMapping("/authors")
+    public String getBooks(Model model) {
+        model.addAttribute("authors", authorService.findAll());
+        return "authors";
+    }
+}
